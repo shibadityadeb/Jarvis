697336d2e8fe6830f87c3877f2f5d6f1ef2c8781
Shibaditya Deb <debshibaditya144@gmail.com>
2025-07-02 14:48:43 +0530

    Authentication setup

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+dotenv.config()
+
+
+const app=express()
+
+const port=process.env.PORT || 5000
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+
+app.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
