e6bdcf2027d8b0228256d58c1b0b64b53dc875cd
Shibaditya Deb <debshibaditya144@gmail.com>
2025-07-03 19:37:19 +0530

    multer setur done and frontend of customization done

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import userRouter from "./routes/user.routes.js"
 dotenv.config()
 
 
@@ -16,6 +17,8 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter)
+
 
 app.listen(port,()=>{
     connectDb()
