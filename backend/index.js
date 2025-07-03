fb261bcbb6cece6cd7d32cc5ba2571f3214f8ffd
Shibaditya Deb <debshibaditya144@gmail.com>
2025-07-03 15:00:00 +0530

    Authentication page setup

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv"
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 dotenv.config()
 
 
 const app=express()
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
